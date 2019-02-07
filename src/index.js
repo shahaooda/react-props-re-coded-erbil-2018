c70a93a46a1ea2f829c2039b2fdfd00b7ac62c44
Shahad Bakoos <shahadbakoos@gmail.com>
2019-02-07 14:29:27 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-// Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './main.css';
+import './fetch-setup';
+
+import App from './components/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
